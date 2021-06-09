e9d85ec2b2a87d14e3fe8c194c2fccde15812d8e
RupaSreeJ <83943024+RupaSreeJ@users.noreply.github.com>
2021-06-09 11:58:16 +0530

    Update App.js

--- src/component/App.js
+++ src/component/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
 import Data from '../mock/MockData'
 import './styles.css'
-import DropDown from '../component/countries-dropdown'
-
-
+import DropDown from './countriesDropDown'
 class App extends Component {
   render() {
     return (                                        
       <div className="App">
-      <h1>{this.props.heading}</h1>
-      <DropDown />
+      <h1>{this.props.Content.heading}</h1>
+      <DropDown Content={this.props.Content} />
       </div>
     );
   }
 }
-App.defaultProps = Data
+App.defaultProps = {Content: Data}
 export default App;
 
