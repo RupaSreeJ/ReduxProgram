8c61e17723202b3a40c96b3c2877c89c23683242
RupaSreeJ <83943024+RupaSreeJ@users.noreply.github.com>
2021-06-09 09:37:22 +0530

    Update RootReducer.js

--- src/reducer/RootReducer.js
+++ src/reducer/RootReducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
 import countriesReducer from './countriesReducer';
 export default combineReducers({
-    event: countriesReducer
+    countriesReducer: countriesReducer
 })
