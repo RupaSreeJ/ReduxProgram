1dfa0990b136811cba067ca01c6755f4de9bba4b
RupaSreeJ <83943024+RupaSreeJ@users.noreply.github.com>
2021-06-09 16:12:04 +0530

    Update App.js

--- src/component/App.js
+++ src/component/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Data from '../mock/MockData'
 import './styles.css'
-import DropDown from './countriesDropDown'
+import CountriesRegionComponent from './countriesDropDown'
 class App extends Component {
   render() {
     return (                                        
       <div className="App">
       <h1>{this.props.Content.heading}</h1>
-      <DropDown Content={this.props.Content} />
+      <CountriesRegionComponent Content={this.props.Content} />
       </div>
     );
   }
