97e2887d00746c5bd696ddbd08645c2c978027b9
RupaSreeJ <83943024+RupaSreeJ@users.noreply.github.com>
2021-06-09 16:12:45 +0530

    Add files via upload

---
+++ src/component/common/commonSelect.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+class CommonSelect extends Component {
+render() {
+        const dropDownList= this.props.dropDownList
+        return (
+            <div>
+                <select  onChange={this.props.onChange} id={this.props.id}>
+                {
+                   dropDownList.map(x => {
+                    return <option>{x.name}</option>
+                  })
+                }
+                </select>
+            </div>
+        )
+    }
+}
+
+export default CommonSelect
+
