656978a456e45d915b75c6894d66154f45709cc0
RupaSreeJ <83943024+RupaSreeJ@users.noreply.github.com>
2021-06-09 11:57:26 +0530

    Update MockData.js

--- src/mock/MockData.js
+++ src/mock/MockData.js
@@ -1,5 +1,22 @@
-const Data = {
-    heading:"Countries App",
-   countries : ['Africa', 'Asia', 'Americas','Europe']
+const Data = 
+{
+   heading:"Countries App",
+   countries : [
+   {
+    id:"africa",
+    name:"Africa"
+   },
+  {
+    id:"americas",
+    name:"Americas"
+  },
+  {
+    id:"asia",
+    name:"Asia"
+  },
+  {
+    id:"europe",
+    name:"Europe"
+  }]
 }
 export default Data;
