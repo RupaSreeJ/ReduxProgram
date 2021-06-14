37a858b6270a50ef3ca3c7870b08df6506cc8b71
RupaSreeJ <83943024+RupaSreeJ@users.noreply.github.com>
2021-06-14 12:25:18 +0530

    Update commonSelect.js

--- src/component/common/commonSelect.js
+++ src/component/common/commonSelect.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 class CommonSelect extends Component {
+    handleOnChange(e) {
+        e.preventDefault();
+        this.props.onChange(e.target.value);
+    }
 render() {
         const dropDownList= this.props.dropDownList
         return (
             <div>
-                <select  onChange={this.props.onChange} id={this.props.id}>
+                <select  onChange={(e)=>this.handleOnChange(e)} id={this.props.id}>
                 {
                    dropDownList.map(x => {
                     return <option>{x.name}</option>
