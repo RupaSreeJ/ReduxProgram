64d68e911cc7dd294fdf34f145459aedf41c2e21
RupaSreeJ <83943024+RupaSreeJ@users.noreply.github.com>
2021-06-24 10:28:36 +0530

    Update countriesReducer.js

--- src/reducer/countriesReducer.js
+++ src/reducer/countriesReducer.js
@@ -1,27 +1,19 @@
 import ACTION_TYPES from '../action/actionTypes'
   const initialState = {
   result: [],                                                       
-  error: null,
+  
 };
 const countriesReducer = (state = initialState, {payload, type}) => {        
   switch (type) {                                       
-    case ACTION_TYPES.FETCH_STARTED: 
-      return {
-        ...state,
-        
-      };
    case ACTION_TYPES.FETCH_SUCCESS:
       return {
         ...state,
           result: payload,  
-        
-        error:null,
-      };
+        };
     case ACTION_TYPES.FETCH_FAIL:
       return {
         ...state,
-        error: payload.error,
-       
+        error:{result:false},
       }; 
     default:
       return state;
