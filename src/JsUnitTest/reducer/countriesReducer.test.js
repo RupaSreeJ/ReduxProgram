4341dda196bd6ca674ddfb245db42ebbfca4690e
RupaSreeJ <83943024+RupaSreeJ@users.noreply.github.com>
2021-06-24 10:26:26 +0530

    Update countriesReducer.test.js

--- src/JsUnitTest/reducer/countriesReducer.test.js
+++ src/JsUnitTest/reducer/countriesReducer.test.js
@@ -1,45 +1,13 @@
 // //import react from "react";
 import ACTION_TYPES from  '../../action/actionTypes';
  import countriesReducer from '../../reducer/countriesReducer';
-// const initialState = {
-//     result: [],                                                       
-//     error: false,
-//   };
-//   describe("event Reducer", ()=>{
-//       it("returns the intial state",() =>{
-//           expect(countriesReducer(undefined, {})).toEqual(initialState);
-//       });
-//       it("handles successful", () => {
-//           expect(countriesReducer(initialState,{
-//               type:ACTION_TYPES.FETCH_SUCCESS,
-//               payload: "India",
-//           })).toEqual({
-//               ...initialState,
-//               result: "India",
 
-//           });
-//       });
-//       it("handles failure", () => {
-//         expect(countriesReducer(initialState,{
-//             type:ACTION_TYPES.FETCH_FAIL,
-//             //payload: "1",
-//             error:{result:false},
-//         })).toEqual({
-//             ...initialState,
-//             //error: "",
-//             error:{result:false},
-
-//         });
-//     });
-//   });
 const initialState = {
     result: [],
     error: false
 }
 describe('testing reducer', () => {
-    // it('returns the initial state', () => {
-    //     expect(countriesReducer(undefined, {})).toEqual(initialState);
-    // });
+   
     it('handles success', () => {
         expect(countriesReducer(initialState, {
             type: ACTION_TYPES.FETCH_SUCCESS,
