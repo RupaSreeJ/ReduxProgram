3ff935f59b6617028ba2b0fcb785df0e1b05e786
RupaSreeJ <83943024+RupaSreeJ@users.noreply.github.com>
2021-06-24 10:22:13 +0530

    Update action-get-countries-test.js

--- src/JsUnitTest/action/action-get-countries-test.js
+++ src/JsUnitTest/action/action-get-countries-test.js
@@ -1,36 +1,40 @@
-// import MockAdapter from "axios-mock-adapter/types";
-// import  {getCountries} from "../../../src/action/action-get-countries"
-         
-// const expectedState = {
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import * as actions from '../../action/action-get-countries';
+const middleware = [thunk];
+const mockStore = configureMockStore(middleware);
+const mock = new MockAdapter(axios);
+const store = mockStore();
 
-// };
-// describe("actions", ()=>{
-//     beforeEach(() =>{
-//         MockAdapter.reset();
-//         mockedStore.clearActions();
-//     });
-//     function success(){
-//         return{
-//             type:"FETCH_SUCCESS",
-//             payload:expectedState,
-//         };
-//     }
-//     function getCountries(){
-//         return (dispatch) => {
-//             return(
-//                 fetch("https://restcountries.eu/rest/v2/region/",{
-//                     method: "GET",
-//                     data: expectedState,
-//                 })
-//                 .then(() => dispatch(success()))
-//             );
-//         };
-//     }
-//     it("should excute getCountries data ", () => {
-//         return mockedStore.dispatch(getCountries()).then(()=>{ 
-//             const actions = mockedStore.getActions();
-//             console.log("actiondata",actions[0]);
-//             expect(actions[0]).toEqual(success());
-//         });
-//     });
-// })
+
+describe('getCountries actions', () => {
+    it('dispatches getCountries after a successfull API requets', () => {
+        mock.onGet("https://restcountries.eu/rest/v2/region/Africa").reply(200, { data: [{ name: "Algeria" }, { name: "Angola" }] })
+        store.dispatch(actions.getCountries("Africa")).then(() => {
+            let expectedActions = [
+                {
+                    type: 'ACTION_TYPES.FETCH_SUCCESS',
+                    payload: data
+                }
+            ]
+            expect(store.getCountries()).toEqual(expectedActions)
+        });
+    });
+
+    it('dispatches FETCH_FAIL after a FAILED API requets', () => {
+        mock.onGet("https://restcountries.eu/rest/v2/region/").reply(400, { error: { message: 'error message' } });
+        store.dispatch(actions.getCountries()).then(() => {
+            let expectedActions = [
+
+                {
+                    type: 'ACTION_TYPES.FETCH_FAIL',
+                    payload: { error: { message: 'error message' } }
+                }
+            ]
+            expect(store.getCountries()).toEqual(expectedActions)
+        });
+    });
+});
+
